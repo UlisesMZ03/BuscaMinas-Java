00cc6e20de4c4033ecbe147465c698a63253a30f
UlisesMZ03 <ulisesmz@estudiantec.cr>
2023-04-18 23:40:06 -0600

    Final-commit

--- ventana busca minas/src/OtherClass.java
+++
@@ -1,7 +0,0 @@
-public class OtherClass {
-    public static void main(String[] args) {
-        ArduinoReceiver arduinoReceiver = new ArduinoReceiver();
-        int variableValue = arduinoReceiver.getVariable();
-        System.out.println("Valor de la variable: " + variableValue);
-    }
-}
